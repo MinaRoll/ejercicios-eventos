e6731f3f54cb9e85222476fb6debb10fc00816a4
romina <romina.torres.b@gmail.com>
2017-05-16 21:12:01 -0400

    ejercicio terminado

---
+++ main.js
@@ -0,0 +1,11 @@
+function carga() {
+    alert("La pagina esta cargada!");
+}
+
+function mouseDentro() {
+     document.getElementById(alert("Estas sobre mi"));
+}
+
+function mouseFuera(){
+	document.getElementById(alert("No estas sobre mi"));
+}
